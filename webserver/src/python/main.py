2bd86ab2cfc905a1046772085ed63042e1b88077
liammccall <liamkm895@gmail.com>
2025-11-18 01:35:51 -0500

    bullshit commit

--- webserver/src/python/main.py
+++ webserver/src/python/main.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return '<img src=/image>'
+    return '<div><img src=/image1><img src=/image2></div>'
 
-@app.route("/image")
-def serve_image():
-    return send_file(build_image("coastline"))
+@app.route("/image1")
+def serve_image_1():
+    return send_file(build_image("rivers_lake_centerlines"))
+
+@app.route("/image2")
+def serve_image_2():
+    return send_file(build_image("land"))
 
 def build_image(name : str):
     return bpl.generate_map(name)
