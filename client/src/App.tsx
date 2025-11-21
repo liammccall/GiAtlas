e999fdc92d5f60418024f1e17e7ea9484fe9ff07
liammccall <liamkm895@gmail.com>
2025-11-20 21:18:30 -0500

    serving

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,14 +7,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src="https://localhost:5002/image1" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/liammccall" target="_blank">
+          <img src="http://localhost:5002/image1" className="logo" alt="Map" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>GiAtlas</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +21,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        GRGIS
       </p>
     </>
   )
