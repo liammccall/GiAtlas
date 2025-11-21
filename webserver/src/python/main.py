ad2736fb681d931739fd9f06812a62302df14b3a
liammccall <liamkm895@gmail.com>
2025-11-21 18:52:21 -0500

    untested dev config

--- webserver/src/python/main.py
+++ webserver/src/python/main.py
@@ -12,7 +12,7 @@ def hello_world():
 
 @app.route("/image1")
 def serve_image_1():
-    return send_file(build_image("rivers_lake_centerlines"))
+    return send_file(build_image("ocean"))
 
 @app.route("/image2")
 def serve_image_2():
