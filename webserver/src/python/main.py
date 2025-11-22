cf7e540d293c38f26309a0953291156849affc48
liammccall <liamkm895@gmail.com>
2025-11-22 18:07:50 -0500

    docker compose up --build --watch

--- webserver/src/python/main.py
+++ webserver/src/python/main.py
@@ -12,7 +12,7 @@ def hello_world():
 
 @app.route("/image1")
 def serve_image_1():
-    return send_file(build_image("ocean"))
+    return send_file(build_image("coastline"))
 
 @app.route("/image2")
 def serve_image_2():
